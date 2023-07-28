5df547c750faa992f90efb893e9ccfb2027adec6
EyalidHasan <eyalidhasanshawon@gmail.com>
2023-07-28 23:15:40 +0600

    my git commit

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"
-  ],
-  plugins: require('flowbite/plugin'),
-  darkMode: "class"
-};
